c9777cd1fc2c7c8cd08b589337dcb16d15a9956d
ash-p-dev <137810533+ash-p-dev@users.noreply.github.com>
2024-07-05 12:08:50 +0530

    Update GetDataServlet.java

--- MySQLWebApp/src/main/java/com/example/servlets/GetDataServlet.java
+++ MySQLWebApp/src/main/java/com/example/servlets/GetDataServlet.java
@@ -1,12 +1,13 @@
 package com.example.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -26,6 +27,7 @@ public class GetDataServlet extends HttpServlet {
         Connection conn = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
+        List<Data> dataList = new ArrayList<>();
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); // Load the MySQL JDBC driver
@@ -35,39 +37,21 @@ public class GetDataServlet extends HttpServlet {
             statement = conn.prepareStatement(sql);
             resultSet = statement.executeQuery();
 
-            PrintWriter out = response.getWriter();
-            out.println("<html><body>");
-            out.println("<h2>Database Data:</h2>");
-            out.println("<button onclick=\"window.location.href='AddUser.jsp';\">Add New User</button>");
-            out.println("<table border='1'>");
-            out.println("<tr><th>ID</th><th>Name</th><th>Address</th><th>Salary</th><th>Actions</th></tr>");
-
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
                 String name = resultSet.getString("name");
                 String address = resultSet.getString("address");
                 String salary = resultSet.getString("salary");
 
-                out.println("<tr>");
-                out.println("<td>" + id + "</td>");
-                out.println("<td>" + name + "</td>");
-                out.println("<td>" + address + "</td>");
-                out.println("<td>" + salary + "</td>");
-                out.println("<td>");
-                out.println("<form action='EditServlet' method='post'>");
-                out.println("<input type='hidden' name='id' value='" + id + "'>");
-                out.println("<input type='submit' value='Edit'>");
-                out.println("</form>");
-                out.println("<form action='DeleteServlet' method='post'>");
-                out.println("<input type='hidden' name='id' value='" + id + "'>");
-                out.println("<input type='submit' value='Delete'>");
-                out.println("</form>");
-                out.println("</td>");
-                out.println("</tr>");
+                Data data = new Data(id, name, address, salary); // Create Data object
+                dataList.add(data); // Add to list
             }
 
-            out.println("</table>");
-            out.println("</body></html>");
+            // Set the list as a request attribute
+            request.setAttribute("dataList", dataList);
+
+            // Forward the request to the JSP
+            request.getRequestDispatcher("/displayData.jsp").forward(request, response);
 
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
